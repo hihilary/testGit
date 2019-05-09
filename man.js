cf186119702d19aa9b15f78fe11be75a20131c94
hihilary <hihilary@outlook.com>
2019-05-09 20:12:35 +0100

    added some codes

--- man.js
+++ man.js
@@ -2,4 +2,8 @@ let helloworld = () => {
   console.log('hello world!');
   console.log('hello world!');
   console.log('hello world!');
+  console.log('hello world!');
+  console.log('issac is a pig!');
+  console.log('hello world!');
+  console.log('hello world!');
 }
